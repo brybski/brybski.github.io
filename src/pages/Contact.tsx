3c0b064466b057cb3fac0a2d4ad8b8b59f50a4ff
Bartlomiej Rybski <b.rybski@qarbon.it>
2025-02-24 09:35:07 +0100

    map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -48,7 +48,7 @@ const Contact = () => {
 
             <div className="w-full h-[400px]">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2573.046726889378!2d20.899802711988187!3d49.841579530897846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473d93a8b7b3c7af%3A0xc1392532ca033bfd!2sOdpocznieszTu!5e0!3m2!1spl!2spl!4v1740354228176!5m2!1spl!2spl"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5588.336268088614!2d20.902383!3d49.841576!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473d93a8b7b3c7af%3A0xc1392532ca033bfd!2sOdpocznieszTu!5e1!3m2!1spl!2spl!4v1740382031281!5m2!1spl!2spl"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
