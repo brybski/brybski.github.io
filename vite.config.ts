ae8ac2781de62d2f57a4f5505cb6f9ef1e70076d
Bartlomiej Rybski <b.rybski@qarbon.it>
2025-02-24 02:20:49 +0100

    Push2.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/brybski.github.io/', // <-- Dodaj tę linijkę
+  base: '/', // <-- Dodaj tę linijkę
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
